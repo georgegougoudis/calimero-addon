f1eb94b5ea889269187c9a9bf393d96f79a103bf
George Gougoudis <g.gougoudis@berdis.gr>
2024-06-08 15:44:41 +0300

    5

--- calimero/run.sh
+++ calimero/run.sh
@@ -2,5 +2,9 @@
 cd "/usr/app/calimero-server-3.0-SNAPSHOT/bin"
 echo "Running Server"
 ls "/usr/app/calimero-server-3.0-SNAPSHOT"
+echo "Running Server"
+ls "/usr/app/calimero-server-3.0-SNAPSHOT/bin"
+echo "Running Server"
 ls "/usr/app"
+echo "Running Server"
 java -cp "./*" io.calimero.server.Launcher /usr/app/server-config.xml
