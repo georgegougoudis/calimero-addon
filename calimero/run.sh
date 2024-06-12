136eba1693d4132e6e58de2f934adc7ac7f0a092
George Gougoudis <g.gougoudis@berdis.gr>
2024-06-12 10:32:52 +0300

    trace

--- calimero/run.sh
+++ calimero/run.sh
@@ -6,4 +6,4 @@ setserial /dev/ttyAMA0 low_latency
 export CALIMERO_SERVER_OPTS="-Dio.calimero.serial.tpuart.maxInterByteDelay=20000"
 
 cat /usr/app/server-config.xml
-./calimero-server --no-stdin  -Djdk.system.logger.level=TRACE /usr/app/server-config.xml
+./calimero-server --no-stdin -vvv -Djdk.system.logger.level=TRACE /usr/app/server-config.xml
